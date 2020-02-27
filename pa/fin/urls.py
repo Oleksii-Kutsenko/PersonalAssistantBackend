63fe985087c82e30ac4983e1d1690a879ea9c5c6
fronimus <kutsenko.new@gmail.com>
2020-02-27 16:41:38 +0200

    FIN-20: swagger has been added

--- pa/fin/urls.py
+++ pa/fin/urls.py
@@ -3,7 +3,9 @@ URLs
 """
 from django.conf.urls import url
 from django.urls import include
-from rest_framework import routers
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import routers, permissions
 from fin import views
 
 router = routers.DefaultRouter()
@@ -11,8 +13,18 @@ router.register(r'accounts', views.AccountViewSet)
 router.register(r'indices', views.IndexViewSet)
 router.register(r'goals', views.GoalViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="PA.FIN API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     url(r'^api/', include(router.urls)),
     url(r'^api/adjusted-index/index/(?P<index_id>[1-9][0-9]*)/$', views.AdjustedIndex.as_view()),
-    url(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
