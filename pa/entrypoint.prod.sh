c9f7d00af6490d9c0abc7d1760476a9916468d18
fronimus <kutsenko.new@gmail.com>
2020-03-30 14:37:23 +0300

    Production entrypoint script has been fixed

--- pa/entrypoint.prod.sh
+++ pa/entrypoint.prod.sh
@@ -10,6 +10,8 @@ if [ "${DATABASE}" = "postgres" ]; then
   echo "PostgreSQL started"
 fi
 
+python3 manage.py collectstatic --no-input --clear
+python3 manage.py migrate
 if pytest && pylint --rcfile=.pylintrc fin/* pa/*; then
   exec "$@"
 else
